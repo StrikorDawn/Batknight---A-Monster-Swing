b09f8a2becfa5da58191d91cd2d2c4d77e106a95
MasterSmeddy <ethanyeldems@gmail.com>
2025-02-18 15:00:47 -0700

    Goblin Can Move and Chase!

--- scripts/goblin.gd
+++ scripts/goblin.gd
@@ -1,26 +1,78 @@
 extends CharacterBody2D
 
+class_name goblin_enemy
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const speed = 50
+var is_goblin_chase: bool = true
 
-@onready var player: Player = $"../Player"
+var health = 80
+var health_max = 80
+var health_min = 0
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+var dead: bool = false
+var taking_damage: bool = false
+var damage_to_deal = 20
+var is_dealing_damage: bool = false
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+var dir: Vector2
+const gravity = 900
+var knockback_force = 200
+var is_roaming: bool = true
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+var player: CharacterBody2D
+var player_in_area = false
 
+
+func _process(delta):
+	if !is_on_floor():
+		velocity.y += gravity * delta
+		velocity.x = 0
+	
+	player = $"../Player"
+	
+	move(delta)
+	handle_animation()
 	move_and_slide()
+
+func move(delta):
+	if !dead:
+		if !is_goblin_chase:
+			velocity += dir * speed * delta
+		elif is_goblin_chase and !taking_damage:
+			var dir_to_player = position.direction_to(player.position) * speed
+			velocity.x = dir_to_player.x
+			dir.x = abs(velocity.x) / velocity.x
+		is_roaming = true
+	elif dead:
+		velocity.x = 0
+
+func handle_animation():
+	var anim_sprite = $AnimatedSprite2D
+	if !dead and !taking_damage and !is_dealing_damage:
+		anim_sprite.play("walk")
+		if dir.x == -1:
+			anim_sprite.flip_h = true
+		elif dir.x == 1:
+			anim_sprite.flip_h = false
+	elif !dead and taking_damage and !is_dealing_damage:
+		anim_sprite.play("hurt")
+		await get_tree().create_timer(0.8).timeout
+		taking_damage = false
+	elif dead and is_roaming:
+		is_roaming = false
+		anim_sprite.play("death")
+		await get_tree().create_timer(1.0).timeout
+		handle_death()
+
+func handle_death():
+	self.queue_free()
+
+func _on_direction_timer_timeout() -> void:
+	$DirectionTimer.wait_time = choose([1.5,2.0,2.5])
+	if !is_goblin_chase:
+		dir = choose([Vector2.RIGHT, Vector2.LEFT])
+		velocity.x = 0
+
+func choose(array):
+	array.shuffle()
+	return array.front()
