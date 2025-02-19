f7ba8b98338d39afb89b3263b7050be930ac8a5e
MasterSmeddy <ethanyeldems@gmail.com>
2025-02-19 15:27:10 -0700

    Updated Goblin Code (Awaiting Player Damage Code for Reference)

--- scripts/enemies/goblin.gd
+++ scripts/enemies/goblin.gd
@@ -16,7 +16,7 @@ var is_dealing_damage: bool = false
 
 var dir: Vector2
 const gravity = 900
-var knockback_force = 200
+var knockback_force = -20
 var is_roaming: bool = true
 
 var player: CharacterBody2D
@@ -42,6 +42,9 @@ func move(delta):
 			var dir_to_player = position.direction_to(player.position) * speed
 			velocity.x = dir_to_player.x
 			dir.x = abs(velocity.x) / velocity.x
+		elif taking_damage:
+			var knockback_dir = position.direction_to(player.position) * knockback_force
+			velocity.x = knockback_dir.x
 		is_roaming = true
 	elif dead:
 		velocity.x = 0
@@ -76,3 +79,17 @@ func _on_direction_timer_timeout() -> void:
 func choose(array):
 	array.shuffle()
 	return array.front()
+
+
+func _on_goblin_melee_hitbox_area_entered(area: Area2D) -> void:
+	#var damage = (Player Damage)
+	#if area == (Player Damage Zone)
+		#take_damage(damage)
+#func take_damage(damage):
+	#health -= damage
+	#taking_damage = true
+	#if health <= health_min:
+		#health = health_min
+		#dead = true
+	#print(str(self), "current health is ", health)
+	pass # Replace with function body.
