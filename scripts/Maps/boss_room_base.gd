edd588b869259c816786a92cbd9972a57e7809b6
Morgan McKay <mckayml922@gmail.com>
2025-03-24 15:14:24 -0600

    Boss!
    jumping stuff and partial progress

--- scripts/Maps/boss_room_base.gd
+++ scripts/Maps/boss_room_base.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 @onready var top_door: TileMapLayer = $TopDoor
+@onready var side_door: TileMapLayer = $SideDoor
 
 @onready var GUILD_CAMP = preload("res://scenes/Maps/guild_camp.tscn")
+const CYCLOPES = preload("res://scenes/enemies/cyclopes.tscn")
 
 func _on_close_door_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
@@ -10,8 +12,26 @@ func _on_close_door_body_exited(body: Node2D) -> void:
 		get_node("CloseDoor").queue_free()
 		
 
-
+func _open_escape_door():
+	side_door.enabled = false
 
 func _on_to_guild_camp_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		Main.load_new_scene(GUILD_CAMP)
+
+
+func _on_start_boss_battle_body_entered(body: Node2D) -> void:
+	
+	if body.is_in_group("Player"):
+		var boss_boi = CYCLOPES.instantiate()
+		add_child(boss_boi)
+		var boss_spawn = get_node("BossSpawn")
+	
+		boss_boi.position = boss_spawn.position
+	
+		boss_boi.start_boss_fight()
+		
+		boss_boi.dead_boss.connect(_open_escape_door)
+	
+		get_node("StartBossBattle").queue_free()
+	
